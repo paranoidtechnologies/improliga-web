12a77933aeec65f93678de655182e32363111390
Pavel Žák <just.paja@gmail.com>
2015-08-05 18:50:33 +0200

    Overwrite x-powered-by

--- src/server/lib/estemiddleware.js
+++ src/server/lib/estemiddleware.js
@@ -8,7 +8,7 @@ export default function addEsteHeaders() {
     // Set X-Powered-By only if it has not been disabled by the user previously.
     if (app.get('x-powered-by'))
       res.set({
-        'X-Powered-By': 'Este.js'
+        'X-Powered-By': 'Improliga'
       });
 
     next();
