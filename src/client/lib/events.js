42e8d273ca0e512b16bd298f70b95bb66e13e9b2
Pavel Žák <just.paja@gmail.com>
2015-10-06 19:37:47 +0200

    Event wakeUp, care for times

--- src/client/lib/events.js
+++ src/client/lib/events.js
@@ -1,13 +1,22 @@
 import moment from 'moment';
 
+const formatDate = 'YYYY-MM-DDTHH:mm:ssZ';
+const formatTime = 'HH:mm:ssZ';
 
 export function wakeUpEvent(item) {
   if (item.start) {
-    item.start = moment(item.start, 'YYYY-MM-DD');
+    item.start = moment(item.start, formatDate);
+    if (item.startTime) {
+      item.startTime = moment(item.startTime, formatTime);
+    }
   }
 
   if (item.end) {
-    item.end = moment(item.end, 'YYYY-MM-DD');
+    item.end = moment(item.end, formatDate);
+
+    if (item.endTime) {
+      item.endTime = moment(item.endTime, formatTime);
+    }
   }
 
   return item;
