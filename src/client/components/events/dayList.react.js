8e567207064b1b687d486cf5ae2359ada7b3a9b5
Pavel Žák <just.paja@gmail.com>
2015-10-15 21:32:20 +0200

    Calendar day list

---
+++ src/client/components/events/dayList.react.js
@@ -0,0 +1,26 @@
+import List from '../browser/list.react';
+import Item from '../browser/item.react';
+import {momentObj} from 'react-moment-proptypes';
+import React from 'react';
+
+export default class EventDayList extends List {
+  static propTypes = {
+    date: momentObj,
+    formatDate: React.PropTypes.string.isRequired,
+  }
+
+  render() {
+    const {date, draw, formatDate, items, msg, pass} = this.props;
+
+    return (
+      <div className="ui-event-day">
+        <div className="title">{date.format(formatDate)}</div>
+        <div className="ui-list-items">
+          {items.map(function(item, key) {
+            return <Item key={item.id} {...{draw, item, items, msg, pass}} />;
+          })}
+        </div>
+      </div>
+    );
+  }
+};
--- src/client/components/events/monthList.react.js
+++ src/client/components/events/monthList.react.js
@@ -1,4 +1,42 @@
 import List from '../browser/list.react';
+import EventDayList from './dayList.react';
+import React from 'react';
 
 export default class EventMonthList extends List {
+
+  render() {
+    const {draw, items, msg, pass} = this.props;
+    const {formatDate} = pass;
+
+    let days = [];
+    let day = null;
+
+    items.forEach(function(item) {
+      const itemDay = item.start.format('YYYY-MM-DD');
+
+      if (!day || day.dateString != itemDay) {
+        day = {
+          date: item.start.clone(),
+          dateString: item.start.format('YYYY-MM-DD'),
+          items: [],
+        }
+
+        days.push(day);
+      }
+
+      day.items.push(item);
+    });
+
+    return (
+      <div className="ui-browser-list">
+        <div className="ui-list-items">
+          {days.map(function(item, key) {
+            const {date, items} = item;
+
+            return <EventDayList key={item.dateString} {...{draw, formatDate, msg, pass, date, items}} />;
+          })}
+        </div>
+      </div>
+    );
+  }
 };
