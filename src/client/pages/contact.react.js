28703ebd16f85c1accc284fb148b5eae454a3c41
Pavel Žák <just.paja@gmail.com>
2015-10-21 20:23:11 +0200

    Contact page, title

--- src/client/pages/contact.react.js
+++ src/client/pages/contact.react.js
@@ -1,3 +1,4 @@
+import DocumentTitle from 'react-document-title';
 import React from 'react';
 import Component from '../components/component.react';
 import ContactForm from '../components/contact/form.react';
@@ -22,38 +23,40 @@ export default class Contact extends Component {
     };
 
     return (
-      <div className="ui-page ui-page-contact">
-        <section className="container ui-section-heads">
-          <div className="text-center">
-            <h1>{msgContact.club.title}</h1>
+      <DocumentTitle title={msgContact.title}>
+        <div className="ui-page ui-page-contact">
+          <section className="container ui-section-heads">
+            <div className="text-center">
+              <h1>{msgContact.club.title}</h1>
 
-            <address>
-              <strong>Česká improvizační liga o. s.</strong><br />
-              Přemyšlenská 1102<br />
-              182 00 Praha 8<br />
+              <address>
+                <strong>Česká improvizační liga o. s.</strong><br />
+                Přemyšlenská 1102<br />
+                182 00 Praha 8<br />
 
-              <abbr title={msgContact.club.icoFull}>{msgContact.club.ico}:</abbr> 26636123
-            </address>
-          </div>
-        </section>
+                <abbr title={msgContact.club.icoFull}>{msgContact.club.ico}:</abbr> 26636123
+              </address>
+            </div>
+          </section>
 
-        <section className="container ui-section-heads">
-          <div className="text-center">
-            <h2>{msgContact.heads.title}</h2>
-            <p>{msgContact.heads.desc}</p>
-          </div>
+          <section className="container ui-section-heads">
+            <div className="text-center">
+              <h2>{msgContact.heads.title}</h2>
+              <p>{msgContact.heads.desc}</p>
+            </div>
 
-          <ul className="row list-unstyled">
-            <li className="col-md-4 text-center">Vanda Gabrielová</li>
-            <li className="col-md-4 text-center">Pavla Sedláčková</li>
-            <li className="col-md-4 text-center">Pavel Žák</li>
-          </ul>
-        </section>
+            <ul className="row list-unstyled">
+              <li className="col-md-4 text-center">Vanda Gabrielová</li>
+              <li className="col-md-4 text-center">Pavla Sedláčková</li>
+              <li className="col-md-4 text-center">Pavel Žák</li>
+            </ul>
+          </section>
 
-        <section className="container ui-section-contact">
-          <ContactForm {...propsForm} />
-        </section>
-      </div>
+          <section className="container ui-section-contact">
+            <ContactForm {...propsForm} />
+          </section>
+        </div>
+      </DocumentTitle>
     );
   }
 };
